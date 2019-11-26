23fc431e437acda03e09266c5c1ab95066b6e399
flybrains <pstock@rockefeller.edu>
2019-11-26 12:42:33 -0500

    initial commit

---
+++ MainWindow.py
@@ -0,0 +1,291 @@
+import os
+import sys
+import cv2
+from odorscape import Canvas
+import numpy as np
+from PyQt5.QtCore import pyqtSlot
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtGui import QImage, QPixmap
+
+cwd = os.getcwd()
+mainWindowCreatorFile = cwd+"/mainwindow.ui"
+
+Ui_MainWindow, QtBaseClass = uic.loadUiType(mainWindowCreatorFile)
+
+class CircularSourceConfigWindow(QtWidgets.QDialog):
+	def __init__(self):
+		super().__init__()
+		self.setWindowTitle("Rectangular Source Configuration")
+		self.setFixedSize(300, 450)
+
+class CanvasSizeConfigWindow(QtWidgets.QDialog):
+	got_values = QtCore.pyqtSignal(int)
+	def __init__(self):
+		super(CanvasSizeConfigWindow, self).__init__()
+		self.l1 = QtWidgets.QLabel('Canvas Height (mm)')
+		self.l2 = QtWidgets.QLabel('Canvas Width (mm)')
+		self.l3 = QtWidgets.QLabel('Resolution (dots/mm)')
+		self.pb = QtWidgets.QPushButton()
+		self.l1Edit = QtWidgets.QSpinBox()
+		self.l1Edit.setMaximum(10000000)
+		self.l1Edit.setValue(1000)
+		self.l2Edit = QtWidgets.QSpinBox()
+		self.l2Edit.setMaximum(10000000)
+		self.l2Edit.setValue(1000)
+		self.l3Edit = QtWidgets.QSpinBox()
+		self.l3Edit.setMaximum(10)
+		self.l3Edit.setValue(1)
+		self.pb.setText("Set Canvas Size")
+		self.grid = QtWidgets.QGridLayout()
+		self.grid.setSpacing(5)
+		self.grid.addWidget(self.l1, 1, 0)
+		self.grid.addWidget(self.l1Edit, 1, 1)
+		self.grid.addWidget(self.l2, 2, 0)
+		self.grid.addWidget(self.l2Edit, 2, 1)
+		self.grid.addWidget(self.l3, 3, 0)
+		self.grid.addWidget(self.l3Edit, 3, 1)
+		self.grid.addWidget(self.pb, 4, 1)
+		self.setLayout(self.grid)
+		self.setGeometry(400, 400, 300, 350 )
+		self.setWindowTitle('Canvas Size Configuration')
+		#self.show()
+		self.pb.clicked.connect(self.store_values)
+		return None
+
+	def store_values(self):
+		self.h = int(self.l1Edit.value())
+		self.w = int(self.l2Edit.value())
+		self.resolution = int(self.l3Edit.value())
+		self.got_values.emit(1)
+		self.close()
+		return None
+
+
+class RectangularSourceConfigWindow(QtWidgets.QDialog):
+	got_values = QtCore.pyqtSignal(int)
+	def __init__(self, canvas):
+		super().__init__()
+
+		self.l1 = QtWidgets.QLabel('Origin X (px)')
+		self.l1Edit = QtWidgets.QSpinBox()
+		self.l1Edit.setRange(-int(canvas.w/2),int(canvas.w/2))
+
+		self.l2 = QtWidgets.QLabel('Origin Y (px)')
+		self.l2Edit = QtWidgets.QSpinBox()
+		self.l2Edit.setMaximum(canvas.h)
+		self.l2Edit.setRange(-int(canvas.h/2),int(canvas.h/2))
+
+		self.l3 = QtWidgets.QLabel('Width (px)')
+		self.l3Edit = QtWidgets.QSpinBox()
+		self.l3Edit.setRange(0,int(canvas.w))
+
+		self.l4 = QtWidgets.QLabel('Height (px)')
+		self.l4Edit = QtWidgets.QSpinBox()
+		self.l4Edit.setRange(0,int(canvas.h))
+
+		self.l5 = QtWidgets.QLabel('Max Value')
+		self.l5Edit = QtWidgets.QSpinBox()
+		self.l5Edit.setMaximum(255)
+		self.l5Edit.setMinimum(0)
+
+		self.l6 = QtWidgets.QLabel('Min Value')
+		self.l6Edit = QtWidgets.QSpinBox()
+		self.l6Edit.setMaximum(255)
+		self.l6Edit.setMinimum(0)
+
+		self.l7 = QtWidgets.QLabel('Max at: ')
+		self.comboBox7 = QtWidgets.QComboBox(self)
+		self.comboBox7.addItem("Top")
+		self.comboBox7.addItem("Bottom")
+		self.comboBox7.addItem("Left")
+		self.comboBox7.addItem("Right")
+
+		self.l8 = QtWidgets.QLabel('Odor Channel')
+		self.comboBox8 = QtWidgets.QComboBox(self)
+		self.comboBox8.addItem("1")
+		self.comboBox8.addItem("2")
+
+		self.pb = QtWidgets.QPushButton()
+
+		self.pb.setText("Add Source")
+		self.grid = QtWidgets.QGridLayout()
+		self.grid.setSpacing(1)
+		self.grid.addWidget(self.l1, 1, 0)
+		self.grid.addWidget(self.l1Edit, 1, 1)
+		self.grid.addWidget(self.l2, 2, 0)
+		self.grid.addWidget(self.l2Edit, 2, 1)
+		self.grid.addWidget(self.l3, 3, 0)
+		self.grid.addWidget(self.l3Edit, 3, 1)
+		self.grid.addWidget(self.l4, 4, 0)
+		self.grid.addWidget(self.l4Edit, 4, 1)
+		self.grid.addWidget(self.l5, 5, 0)
+		self.grid.addWidget(self.l5Edit, 5, 1)
+		self.grid.addWidget(self.l6, 6, 0)
+		self.grid.addWidget(self.l6Edit, 6, 1)
+		self.grid.addWidget(self.l7, 7, 0)
+		self.grid.addWidget(self.comboBox7, 7, 1)
+		self.grid.addWidget(self.l8, 8, 0)
+		self.grid.addWidget(self.comboBox8, 8, 1)
+		self.grid.addWidget(self.pb, 9, 1)
+		self.setLayout(self.grid)
+		self.setGeometry(400, 400, 300, 500)
+		self.setWindowTitle("Rectangular Source Configuration")
+		self.pb.clicked.connect(self.store_values)
+
+		self.show()
+
+		return None
+
+class CircularSourceConfigWindow(QtWidgets.QDialog):
+	got_values = QtCore.pyqtSignal(int)
+	def __init__(self, canvas):
+		super().__init__()
+
+		self.l1 = QtWidgets.QLabel('Origin X (px)')
+		self.l1Edit = QtWidgets.QSpinBox()
+		self.l1Edit.setRange(-int(canvas.w/2),int(canvas.w/2))
+
+		self.l2 = QtWidgets.QLabel('Origin Y (px)')
+		self.l2Edit = QtWidgets.QSpinBox()
+		self.l2Edit.setMaximum(canvas.h)
+		self.l2Edit.setRange(-int(canvas.h/2),int(canvas.h/2))
+
+		self.l3 = QtWidgets.QLabel('Radius (px)')
+		self.l3Edit = QtWidgets.QSpinBox()
+		self.l3Edit.setRange(0,int(canvas.w/2))
+
+		self.l4 = QtWidgets.QLabel('Max Value')
+		self.l4Edit = QtWidgets.QSpinBox()
+		self.l4Edit.setMaximum(255)
+		self.l4Edit.setMinimum(0)
+
+		self.l5 = QtWidgets.QLabel('Min Value')
+		self.l5Edit = QtWidgets.QSpinBox()
+		self.l5Edit.setMaximum(255)
+		self.l5Edit.setMinimum(0)
+
+		self.l6 = QtWidgets.QLabel('Odor Channel')
+		self.comboBox6 = QtWidgets.QComboBox(self)
+		self.comboBox6.addItem("1")
+		self.comboBox6.addItem("2")
+
+		self.pb = QtWidgets.QPushButton()
+
+		self.pb.setText("Add Source")
+		self.grid = QtWidgets.QGridLayout()
+		self.grid.setSpacing(1)
+		self.grid.addWidget(self.l1, 1, 0)
+		self.grid.addWidget(self.l1Edit, 1, 1)
+		self.grid.addWidget(self.l2, 2, 0)
+		self.grid.addWidget(self.l2Edit, 2, 1)
+		self.grid.addWidget(self.l3, 3, 0)
+		self.grid.addWidget(self.l3Edit, 3, 1)
+		self.grid.addWidget(self.l4, 4, 0)
+		self.grid.addWidget(self.l4Edit, 4, 1)
+		self.grid.addWidget(self.l5, 5, 0)
+		self.grid.addWidget(self.l5Edit, 5, 1)
+		self.grid.addWidget(self.l6, 7, 0)
+		self.grid.addWidget(self.comboBox6, 7, 1)
+		self.grid.addWidget(self.pb, 9, 1)
+		self.setLayout(self.grid)
+		self.setGeometry(400, 400, 300, 500)
+		self.setWindowTitle("Circular Source Configuration")
+		self.pb.clicked.connect(self.store_values)
+
+		self.show()
+
+		return None
+
+
+	def store_values(self):
+		self.x = int(self.l1Edit.value())
+		self.y = int(self.l2Edit.value())
+		self.w = int(self.l3Edit.value())
+		self.h = int(self.l4Edit.value())
+		self.max = int(self.l5Edit.value())
+		self.min = int(self.l6Edit.value())
+		self.maxat = str(self.comboBox7.currentText())
+		self.channel = str(self.comboBox8.currentText())
+		self.got_values.emit(1)
+		self.close()
+		return None
+
+class Odorscape(QtWidgets.QMainWindow, Ui_MainWindow):
+	def __init__(self):
+		QtWidgets.QMainWindow.__init__(self)
+		Ui_MainWindow.__init__(self)
+		self.setupUi(self)
+		self.setWindowTitle('Odorscape')
+		self.setFixedSize(self.size())
+		self.canvas = Canvas()
+		self.actionNewCanvas.triggered.connect(self.initializeCanvas)
+		self.canvasconfigwindow = CanvasSizeConfigWindow()
+		self.canvasconfigwindow.got_values.connect(self.displayCanvas)
+		self.rectangularGradientButton.clicked.connect(self.initializeRectBuilder)
+		self.circularGradientButton.clicked.connect(self.initializeCircleBuilder)
+
+
+	@pyqtSlot(QImage)
+	def setCanvas(self, image):
+		image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+		image = image.copy()
+		qimage = QImage(image, image.shape[0], image.shape[1], QImage.Format_RGB888)
+		pixmap = QPixmap(qimage)
+		pixmap = pixmap.scaled(700,700)
+		self.canvasLabel.setPixmap(pixmap)
+		self.canvasLabel.show()
+
+	def initializeCanvas(self):
+		self.canvasconfigwindow.show()
+		return None
+
+	def displayCanvas(self, init=True):
+		if init:
+			self.canvas = Canvas(self.canvasconfigwindow.w, self.canvasconfigwindow.h, self.canvasconfigwindow.resolution)
+		self.canvasImage = self.canvas.build_canvas()
+		self.setCanvas(self.canvasImage)
+
+	def initializeRectBuilder(self, show=False):
+		self.rectconfigwindow = RectangularSourceConfigWindow(self.canvas)
+		self.rectconfigwindow.got_values.connect(self.addRectangularGradient)
+
+	def initializeCircleBuilder(self, show=False):
+		self.circleconfigwindow = CircularSourceConfigWindow(self.canvas)
+		self.circleconfigwindow.got_values.connect(self.addCircularGradient)
+
+	def addRectangularGradient(self):
+		x = self.rectconfigwindow.x
+		y = self.rectconfigwindow.y
+		h = self.rectconfigwindow.h
+		w = self.rectconfigwindow.w
+		max = self.rectconfigwindow.max
+		min = self.rectconfigwindow.min
+		channel = self.rectconfigwindow.channel
+		maxat = self.rectconfigwindow.maxat
+
+		self.canvas.add_square_gradient(x,y,w,h,max, min, channel, maxat=maxat)
+		self.canvas.check_and_correct_overlap()
+		self.displayCanvas(init=False)
+		return None
+
+	def addCircularGradient(self):
+		x = self.rectconfigwindow.x
+		y = self.rectconfigwindow.y
+		r = self.rectconfigwindow.r
+		max = self.rectconfigwindow.max
+		min = self.rectconfigwindow.min
+		channel = self.rectconfigwindow.channel
+
+		self.canvas.add_circular_gradient(x,y,w,h,max, min, channel, maxat=maxat)
+		self.canvas.check_and_correct_overlap()
+		self.displayCanvas(init=False)
+		return None
+
+
+
+
+if __name__ == "__main__":
+	app = QtWidgets.QApplication(sys.argv)
+	window = Odorscape()
+	window.show()
+	sys.exit(app.exec_())
---
+++ odorscape.py
@@ -0,0 +1,124 @@
+import numpy as np
+import cv2
+
+def convert_canvas_to_bitmap(canvas):
+    label = QLabel()
+    pixmap = QPixmap('path_to_your_image')
+    label.setPixmap(pixmap)
+
+    height, width, channel = cvImg.shape
+    bytesPerLine = 3 * width
+    qImg = QImage(cvImg.data, width, height, bytesPerLine, QImage.Format_RGB888)
+
+def load_canvas(canvas_address):
+    canvas = np.load(canvas_address)
+    return canvas
+
+def save_canvas(canvas, canvas_address):
+    np.save(canvas, canvas_address)
+    return None
+
+
+class Canvas(object):
+    def __init__(self, w=1000, h=1000, resolution=2):
+        self.w = w
+        self.h = h
+        self.airchannel = np.zeros((h, w), dtype=np.int16)
+        self.channel1 = np.zeros((h, w), dtype=np.int16)
+        self.channel2 = np.zeros((h, w), dtype=np.int16)
+
+    def build_canvas(self):
+        self.canvas = np.zeros((self.h, self.w, 3))
+        self.canvas[:,:,0] = self.airchannel
+        self.canvas[:,:,1] = self.channel1
+        self.canvas[:,:,2] = self.channel2
+        self.canvas = self.canvas.astype(np.uint8)
+        return self.canvas
+
+    def add_circular_gradient(self, x, y, r, max, min, channel):
+        patch = np.zeros((2*r, 2*r))
+        center = np.array([r,r])
+        for i in range(2*r):
+            for j in range(2*r):
+                dist = np.linalg.norm(np.array([i,j]) - center)
+                if dist <= r:
+                    patch[i,j] = (1-(dist/r))*(max - min)
+        patch = patch.astype(np.int16)
+        if channel==1:
+            self.channel1[(y-r):(y+r), (x-r):(x+r)] += patch[:,:]
+        if channel==2:
+            self.channel2[(y-r):(y+r), (x-r):(x+r)] += patch[:,:]
+
+
+    def add_square_gradient(self, x, y, w, h, max, min, channel, maxat='Top'):
+
+        if maxat=='Top':
+            strand = np.linspace(max, min, num=h)
+            tup = tuple([strand for i in range(w)])
+            layer = np.column_stack(tup)
+        if maxat=='Bottom':
+            strand = np.linspace(min, max, num=h)
+            tup = tuple([strand for i in range(w)])
+            layer = np.column_stack(tup)
+        if maxat=='Left':
+            strand = np.linspace(max, min, num=w)
+            tup = tuple([strand for i in range(h)])
+            layer = np.row_stack(tup)
+        if maxat=='Right':
+            strand = np.linspace(min, max, num=w)
+            tup = tuple([strand for i in range(h)])
+            layer = np.row_stack(tup)
+
+        # Replace canvas patch with gradient
+        layer = layer.astype(np.int16)
+
+        if channel=='1':
+            self.channel1[y:(y+h), x:(x+w)] += layer[:,:]
+
+        if channel=='2':
+            self.channel2[y:(y+h), x:(x+w)] += layer[:,:]
+
+
+    def check_and_correct_overlap(self):
+
+        composite = np.add(self.channel1, self.channel2)
+        problem_row = list(np.where(composite>255)[0])
+        problem_col = list(np.where(composite>255)[1])
+        problem_coords = [[problem_row[i], problem_col[i]] for i in range(len(problem_row))]
+
+        for point in problem_coords:
+            sum = self.channel1[point[0], point[1]]+self.channel2[point[0], point[1]]
+
+            ch1_val = int((self.channel1[point[0], point[1]]/sum)*255)
+            ch2_val = int((self.channel2[point[0], point[1]]/sum)*255)
+
+            self.channel1[(point[0]),(point[1])] = ch1_val
+            self.channel2[(point[0]),(point[1])] = ch2_val
+
+        self.airchannel = 255*np.ones(self.channel1.shape) - (self.channel1 + self.channel2)
+
+        self.build_canvas()
+
+        # cv2.imshow('g', self.canvas)
+        # cv2.waitKey(0)
+
+        return None
+
+if __name__ == "__main__":
+    canvas = Canvas(600,600)
+    canvas.add_square_gradient(200, 0, 200, 600, 255, 0, 1, maxat='top')
+    canvas.add_circular_gradient(400, 400, 100, 255, 0, 2)
+    canvas.add_circular_gradient(200, 400, 100, 255, 0, 1)
+
+    canvas.check_and_correct_overlap()
+    cv2.imshow('grad1', canvas.canvas)
+    cv2.waitKey(0)
+
+
+
+
+
+
+
+
+#
